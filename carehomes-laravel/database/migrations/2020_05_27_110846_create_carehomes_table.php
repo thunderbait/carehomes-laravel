2a6228ac8bdb6da3ae72e45ac92c509ef5e6a191
Giorgos Andreou <gandreou@alacrityfoundation.com>
2020-05-27 12:18:59 +0100

    Create 2020_05_27_110846_create_carehomes_table.php

---
+++ carehomes-laravel/database/migrations/2020_05_27_110846_create_carehomes_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCarehomesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('carehomes', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('name');
+            $table->integer('number_beds');
+            $table->integer('location_id');
+            $table->integer('group_id');
+            $table->integer('type_id');
+            $table->longText('notes');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('carehomes');
+    }
+}
