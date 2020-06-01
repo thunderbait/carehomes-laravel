9619580c3bc71186e0bee83acfd17b9fdda68b3a
Sotiris Hadjipanayi <sothadjipanayi@gmail.com>
2020-06-01 17:00:25 +0100

    Update 2020_05_27_112002_create_locations_table.php

--- carehomes-laravel/database/migrations/2020_05_27_112002_create_locations_table.php
+++ carehomes-laravel/database/migrations/2020_05_27_112002_create_locations_table.php
@@ -15,8 +15,8 @@ class CreateLocationsTable extends Migration
     {
         Schema::create('locations', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('local_authority');
+            $table->longtext('name');
+            $table->longtext('local_authority');
             $table->timestamps();
         });
     }
