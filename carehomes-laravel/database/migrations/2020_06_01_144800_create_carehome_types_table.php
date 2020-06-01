c106ef9709912e48561fb783916859ec7850ba04
Emily Fothergill <efothergill@alacrityfoundation.com>
2020-06-01 15:55:17 +0100

    pivot migrations added

--- carehomes-laravel/database/migrations/2020_06_01_144800_create_carehome_types_table.php
+++ carehomes-laravel/database/migrations/2020_06_01_144800_create_carehome_types_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateCarehomesTable extends Migration
+class CreateCarehomeTypesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,15 +13,11 @@ class CreateCarehomesTable extends Migration
      */
     public function up()
     {
-        Schema::create('carehomes', function (Blueprint $table) {
+        Schema::create('carehome_types', function (Blueprint $table) {
             $table->id();
+            $table->integer('carehome_id');
+            $table->integer('type_id');
             $table->timestamps();
-            $table->longText('name');
-            $table->integer('number_beds');
-            $table->integer('location_id');
-            $table->integer('group_id');
-            $table->longText('notes');
-
         });
     }
 
@@ -32,6 +28,6 @@ class CreateCarehomesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('carehomes');
+        Schema::dropIfExists('carehome_types');
     }
 }
