677a570a09d64fdf0e01b1ee86266567dfc2ae54
Sotiris Hadjipanayi <sothadjipanayi@gmail.com>
2020-06-15 15:23:25 +0100

    Migrations & Model for Local Authorities Table

--- carehomes-laravel/database/migrations/2020_06_15_141857_create_local_authorities_table.php
+++ carehomes-laravel/database/migrations/2020_06_15_141857_create_local_authorities_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateLocationsTable extends Migration
+class CreateLocalAuthoritiesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,9 @@ class CreateLocationsTable extends Migration
      */
     public function up()
     {
-        Schema::create('locations', function (Blueprint $table) {
+        Schema::create('local_authorities', function (Blueprint $table) {
             $table->id();
             $table->longtext('name');
-            $table->integer('local_authority_id');
             $table->timestamps();
         });
     }
@@ -28,6 +27,6 @@ class CreateLocationsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('locations');
+        Schema::dropIfExists('local_authorities');
     }
 }
