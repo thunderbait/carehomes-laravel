9b4d3dd24effd54efefeb51cb8e17e653eca2369
Sotiris Hadjipanayi <sothadjipanayi@gmail.com>
2020-06-16 11:45:57 +0100

    Update CarehomeController.php

--- carehomes-laravel/app/Http/Controllers/CarehomeController.php
+++ carehomes-laravel/app/Http/Controllers/CarehomeController.php
@@ -60,24 +60,33 @@ class CarehomeController extends Controller
         $local_auth = $request->get('local_authority');
         $number_beds = $request->get('number_beds');
 
-        /// $local_auth_id =  Query - From local_authorities table get the id of the entry with name $local_auth 
+        /// From the local authorities table I want to get the id for the entry
+        /// that has a name = $local_auth
+        /// Expected to return only 1 value (interger)
         $local_auth_id = DB::table('local_authorities')
             ->where('name', 'LIKE', "%{$local_auth}")
-            ->get(['id']);  
-        /// $local_auth_locations = [From locations table get the id's of all carehomes with local_auhority_id = $local_auth_id]
+            ->get(['id']);
+
+
+        /// From the locations table, find all the locations with local_authority_id = $local_auth_id.
+        /// Since the relationship between local_authorities and locations is One-to-Many
+        /// i want to input the results into an array called $local_auth_locations
         $local_auth_locations =  DB::table('locations')
             ->orderBy('name')
-            ->where('local_auhority_id', 'LIKE', "%{local_auth_id}")
+            ->where('local_auhority_id', 'LIKE', "%{$local_auth_id}")
             ->map(function ($a) {
                 return $a;
             });
-        /// Query the carehomes table
+
+        /// Query the carehomes table, to find all carehomes with a location_id that is in $local_auth_locations 
+        /// I was thinking i could use the if(in_array) method
+        /// Then, filter out all the carehomes with less than ($number_beds) beds
         /// SELECT * FROM `carehomes-db`.carehomes where number_beds>$number_beds AND location_id is in $local_auth_locations;
         $carehomes_query = DB::table('carehomes')
-            ->where('number_beds', '>', "%{number_beds}")
+            ->where('number_beds', '>', "%{$number_beds}")
             ->
 
-
+        /// At the end, I want to display the results in a view with the same layout as /carehomes/index.blade.php
     }
 
 }
