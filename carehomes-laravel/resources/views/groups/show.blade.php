b59479ddc4039ba5da5ebfa437b731ef66680a8a
Sotiris Hadjipanayi <sothadjipanayi@gmail.com>
2020-06-30 16:11:20 +0100

    Quick Edits

--- carehomes-laravel/resources/views/groups/show.blade.php
+++ carehomes-laravel/resources/views/groups/show.blade.php
@@ -11,47 +11,33 @@
         </nav>
     </div>
 
-<div class="container">
-
-        <div class="jumbotron jumbotron-fluid">
-            <div class="container">
-                <h1 class="display-4">{{$group->name}}</h1>
-            </div>
-        </div>        
-
-        <div class="row">
-            @if(session()->get('success'))
-                <div class="alert alert-success">
-                    {{ session()->get('success') }}
-                </div><br/>
-            @endif
+    <div class="jumbotron jumbotron-fluid">
+        <div class="container">
+            <h1 class="display-4">{{$group->name}}</h1>
         </div>
+    </div>        
+
+    <div class="row">
+        @if(session()->get('success'))
+            <div class="alert alert-success">
+                {{ session()->get('success') }}
+            </div><br/>
+        @endif
+    </div>
+
+    <div class="container">
 
         <h2>Carehomes</h2>
+
         <div class="row">
-            <table class="table table-striped">
-                <thead>
-                <tr>
-                    <th>ID</th>
-                    <th>Name</th>
-                    <th>Location</th>
-                    <th>No.Beds</th>
-                    <th>Notes</th>
-                </tr>
-                </thead>
-                <tbody>
-                     @foreach($group->carehomes as $carehome)
-                	<tr>
-                        <td>{{$carehome->id}}</td>
-                        <td><a href="{{route('carehomes.show', $carehome->id)}}">{{$carehome->name}}</a></td>
-                        <td>{{$carehome->location->name}}</td>
-                        <td>{{$carehome->number_beds}}</td>
-                        <td>{{$carehome->notes}}</td>
-                    </tr>
-                    @endforeach
-                </tbody>
-            </table>
-        </div>        
-</div>
+            @foreach($group->carehomes as $carehome)
+            <div class="col-4 col-sm-12 col-md-4 col-xl-4">
+                @include('groups.widgets.carehomes')
+            </div>
+            @endforeach
+        </div>
+
+    </div>
+
 
 @endsection
