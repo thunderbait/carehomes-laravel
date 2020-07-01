1088d9a338946e7a0993327cefa7006256218029
Emily Fothergill <efothergill@alacrityfoundation.com>
2020-07-01 12:10:43 +0100

    Filter works if everything except specialisms is filled in

--- carehomes-laravel/app/Http/Controllers/CarehomeController.php
+++ carehomes-laravel/app/Http/Controllers/CarehomeController.php
@@ -11,12 +11,11 @@ use Illuminate\Http\Request;
 
 class CarehomeController extends Controller
 {
-	public function index(Request $request)
-	{
-        $query = Carehome::with('location')->orderBy('name', 'asc');
+    public function index(Request $request)
+    {
+        $query = Carehome::with('group')->orderBy('name', 'asc');
 
-        if ($searchTerm = $request->query('q'))
-        {
+        if ($searchTerm = $request->query('q')) {
             $query->where('name', 'LIKE', "%{$searchTerm}%");
         }
 
@@ -30,12 +29,12 @@ class CarehomeController extends Controller
         $specialisms = Specialism::orderBy('name', 'asc')->get();
 
         return view('carehomes.index', compact('carehomes', 'local_authorities', 'groups', 'types', 'specialisms'));
-	}
+    }
 
     public function show($id)
     {
         $carehome = Carehome::findOrFail($id);
-        $return = (($carehome->number_beds * 365)*(92.5/100))*(65/100);
+        $return = (($carehome->number_beds * 365) * (92.5 / 100)) * (65 / 100);
         return view('carehomes.show', compact('carehome'));
     }
 
@@ -51,16 +50,22 @@ class CarehomeController extends Controller
         $specialism2 = $request->query('specialism2');
         $specialism3 = $request->query('specialism3');
 
-        if ($localAuthority && $group && $minBeds && $type1 && $type2 && $type3 && $specialism1 && $specialism2 && $specialism3)
-        {
+        if ($localAuthority && $group && $minBeds && $type1 && $type2 && $type3 && $specialism1 && $specialism2 && $specialism3) {
             return $query->where('number_beds', '>=', $minBeds)
                 ->where('group_id', $group)
-                ->whereHas('location', function($query) use ($localAuthority) {
+                ->whereHas('location', function ($query) use ($localAuthority) {
                     $query->where('local_authority_id', $localAuthority);
+                })
+                ->whereHas('types', function ($query) use ($type1, $type2, $type3) {
+                    $query->where('type_id', $type1)
+                        ->orWhere('type_id', $type2)
+                        ->orWhere('type_id', $type3);
+                })
+                ->orWhereHas('specialisms', function ($query) use ($specialism1, $specialism2, $specialism3) {
+                    $query->where('specialism_id', $specialism1)
+                        ->orWhere('specialism_id', $specialism2)
+                        ->orWhere('specialism_id', $specialism3);
                 });
-
-                //->where('carehome_types.type_id', $type1 || $type2 || $type3)
-                //->where('carehome_specialisms.specialism_id', $specialism1 || $specialism2 || $specialism3);
         }
     }
 }
